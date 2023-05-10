52c1c2f1c5a967c963f18859b14bdce61b629eb7
FabrizioFavia <personal.favia@gmail.com>
2023-05-10 17:40:14 +0200

    feat(html/css/js): cleaned up code

--- js/main.js
+++ js/main.js
@@ -1,48 +1,48 @@
-const {createApp} =  Vue
+const { createApp } = Vue
 
 createApp({
     data() {
-      return {
-         newTaskModel: {
-            text: "",
-            status: false,
-        }, 
+        return {
+            newTaskModel: {
+                text: "",
+                status: false,
+            },
 
-       tasks: [
-        {
-            text: "Fai la spesa",
-            status: false,
-        },
-        {
-            text: "Lava la macchina",
-            status: false,
-        },
-        {
-            text: "Invia la mail",
-            status: false,
+            tasks: [
+                {
+                    text: "Fai la spesa",
+                    status: false,
+                },
+                {
+                    text: "Lava la macchina",
+                    status: false,
+                },
+                {
+                    text: "Invia la mail",
+                    status: false,
+                }
+            ]
         }
-       ]
-      }
     },
     methods: {
 
-        addTask(){
-            let newTask = {...this.newTaskModel};
+        addTask() {
+            let newTask = { ...this.newTaskModel };
             console.log(newTask);
             if (newTask.text.length > 3) {
                 this.tasks.unshift(newTask);
                 this.newTaskModel.text = "";
-            } 
+            }
         },
 
-        removeTask(i){
-            this.tasks.splice(i,1);
+        removeTask(i) {
+            this.tasks.splice(i, 1);
         },
 
-        taskCheck(i){
-           console.log(this.tasks[i].status)
-           this.tasks[i].status = !this.tasks[i].status;
-           console.log(this.tasks[i].status)
+        taskCheck(i) {
+            console.log(this.tasks[i].status)
+            this.tasks[i].status = !this.tasks[i].status;
+            console.log(this.tasks[i].status)
         },
     },
-  }).mount('#app')
+}).mount('#app')
